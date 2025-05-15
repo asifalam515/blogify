4d6e6b5482e975cf8a8767e6b22e0421807666d2
asifalam515 <asibulalam515@gmail.com>
2025-05-15 22:32:25 +0600

    blog route created

--- blogify_backend/src/features/Blog/blogRoute.ts
+++ blogify_backend/src/features/Blog/blogRoute.ts
@@ -3,5 +3,12 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 // router for post a blog
 router.post("/create-blog", async (req: Request, res: Response) => {});
-
+// get all blogs
+router.get("/", async (req: Request, res: Response) => {});
+// get single blog
+router.get("/:id", async (req: Request, res: Response) => {});
+// update a blog
+router.put("/:id", async (req: Request, res: Response) => {});
+// delete a blog
+router.delete("/:id", async (req: Request, res: Response) => {});
 export const blogRouter = router;
